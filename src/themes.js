a413e06789779bc4e4db54a16dea7a643a7daa66
Alex Tsai <caffodian@users.noreply.github.com>
2018-11-27 12:51:36 -0800

    add basic themes file

---
+++ src/themes.js
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    primary: "blue",
+    secondary: "red",
+    text: "white"
+  },
+  buttons: {
+    primary: {
+      color: "text",
+      backgroundColor: "primary",
+      "&:hover": {
+        backgroundColor: "black"
+      }
+    }
+  }
+};
+
+export default theme;
